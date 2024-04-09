ba30698fe2cf816a5c630e1b508b1fd551e8904a
IanGs1 <goldfeld35@gmail.com>
2024-04-08 21:46:58 -0300

    feat: create an user useCase (it's not in a specific folder, yet)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+import z from "zod";
+import prisma from "./lib/prisma";
 
 export const app = fastify();
+
+app.post("/users", async (request: FastifyRequest, reply: FastifyReply) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = createUserSchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return reply.status(201).send();
+})
