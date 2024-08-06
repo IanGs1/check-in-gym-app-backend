499f2a67927199b3a62eb86336118efa30200431
IanGs1 <goldfeld35@gmail.com>
2024-08-05 22:38:18 -0300

    feat: find nearby gyms route & search gym by name route

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,15 @@ import { FastifyInstance } from "fastify";
 
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 
+import { create } from "./create-controller";
+import { search } from "./search-controller";
+import { nearby } from "./nearby-controller";
+
 export async function gymRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.post("/gyms", create);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
 };
