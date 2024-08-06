ec6a9e1aaabeabda1acb69bdc6a864486fa271cb
IanGs1 <goldfeld35@gmail.com>
2024-08-06 14:09:52 -0300

    folders: creating an app routes file

---
+++ src/http/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+import { userRoutes } from "./controllers/users/routes";
+import { gymRoutes } from "./controllers/gyms/routes";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.register(userRoutes);
+
+  app.register(gymRoutes);
+};
