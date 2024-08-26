eeb4474471e272e138ee1e19bebbcfa97669f30f
IanGs1 <goldfeld35@gmail.com>
2024-08-25 21:04:06 -0300

    fix: verify-user-role middleware

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -5,16 +5,14 @@ import { FastifyInstance } from "fastify";
 import request from "supertest";
 
 export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false) {  
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
       name: "Jonh Doe",
-      email: "jonhdoe@example.com",
+      email: "jonh.doe@example.com",
       password_hash: await hash("123456", 7),
       role: isAdmin ? "ADMIN" : "MEMBER",
     }
   });
-
-  // console.log(user);
 
   const authResponse = await request(app.server)
   .post("/sessions")
